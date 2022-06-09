edfa5a07798f08647f3baf7acbd31ec4168044ba
Sakib Ahamed Khan <96885912+SakibAhamedKhan@users.noreply.github.com>
2022-06-09 20:59:10 +0600

    Let start

---
+++ src/Components/Table.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Table = () => {
+    return (
+        <div className=' px-4 md:px-8 lg:px-10'>
+            <h2 className='my-5 text-2xl font-semibold'>My Todo Table</h2>
+            <div class="overflow-x-auto w-full">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" class="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" class="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div class="flex items-center space-x-3">
+                                    <div class="avatar">
+                                        <div class="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="font-bold">Hart Hagerty</div>
+                                        <div class="text-sm opacity-50">United States</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <br />
+                                <span class="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                            </td>
+                            <td>Purple</td>
+                            <th>
+                                <button class="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr> */}
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Table;
